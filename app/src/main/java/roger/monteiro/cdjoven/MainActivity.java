e0f30a03f526f37ef8ee6950cff40744faa85beb
RogerMonteiro124 <rogermonteiro15@outlook.com>
2017-02-10 21:52:47 -0400

    Adcionado arquivos de audios , iniciado processo de desenvolvimento do menu inicial e aplicado logica em todos os botões.

--- app/src/main/java/roger/monteiro/cdjoven/MainActivity.java
+++ app/src/main/java/roger/monteiro/cdjoven/MainActivity.java
@@ -4,49 +4,127 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.media.MediaPlayer;
-import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     Context context = this;
-    MediaPlayer mp,mp2;
+    MediaPlayer EuCreio,Consagracao,UltimoConvite,MinhaFortaleza,Orando,DocePaz,OucoSuaVoz;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mp = MediaPlayer.create(context, R.raw.sound);
-        mp2 = MediaPlayer.create(context, R.raw.sound2);
-        final Button b = (Button) findViewById(R.id.Button);
-        final Button c = (Button) findViewById(R.id.Button2);
-        final Button stop = (Button) findViewById(R.id.stop);
 
-        b.setOnClickListener(new View.OnClickListener() {
+
+        EuCreio=MediaPlayer.create(context,R.raw.eucreio);
+        Consagracao=MediaPlayer.create(context,R.raw.consagracao);
+        UltimoConvite=MediaPlayer.create(context,R.raw.ultimoconvite);
+        MinhaFortaleza=MediaPlayer.create(context,R.raw.minhafortaleza);
+        Orando=MediaPlayer.create(context,R.raw.orando);
+        DocePaz=MediaPlayer.create(context,R.raw.docepaz);
+        OucoSuaVoz=MediaPlayer.create(context,R.raw.oucosuavoz);
+
+
+        final ImageButton btnEuCreio = (ImageButton) findViewById(R.id.eucreio);
+        final ImageButton btnConsagracao = (ImageButton) findViewById(R.id.consagracao);
+        final ImageButton btnUltimoConvite = (ImageButton) findViewById(R.id.ultimoconvite);
+        final ImageButton btnMinhaFortaleza = (ImageButton) findViewById(R.id.minhafortaleza);
+        final ImageButton btnOrando = (ImageButton) findViewById(R.id.orando);
+        final ImageButton btnDocePaz = (ImageButton) findViewById(R.id.docepaz);
+        final ImageButton btnOucoSuaVoz = (ImageButton) findViewById(R.id.oucosuavoz);
+        final Button stop =(Button)findViewById(R.id.stop);
+
+        btnEuCreio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 try {
-                    if (mp.isPlaying()) {
-                        mp.stop();
-                        mp.release();
-                        mp = MediaPlayer.create(context, R.raw.sound);
-                    } mp.start();
+                    if (EuCreio.isPlaying()) {
+                        EuCreio.stop();
+                        EuCreio.release();
+                        EuCreio=MediaPlayer.create(context,R.raw.eucreio);
+                    } EuCreio.start();
                 } catch(Exception e) { e.printStackTrace(); }
             }
         });
-        c.setOnClickListener(new View.OnClickListener() {
+        btnConsagracao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 try {
-                    if (mp2.isPlaying()) {
-                        mp2.stop();
-                        mp2.release();
-                        mp2 = MediaPlayer.create(context, R.raw.sound2);
-                    } mp2.start();
+                    if (Consagracao.isPlaying()) {
+                        Consagracao.stop();
+                        Consagracao.release();
+                        Consagracao=MediaPlayer.create(context,R.raw.consagracao);
+                    } Consagracao.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        btnUltimoConvite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    if (UltimoConvite.isPlaying()) {
+                        UltimoConvite.stop();
+                        UltimoConvite.release();
+                        UltimoConvite=MediaPlayer.create(context,R.raw.ultimoconvite);
+                    } UltimoConvite.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        btnMinhaFortaleza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    if (MinhaFortaleza.isPlaying()) {
+                        MinhaFortaleza.stop();
+                        MinhaFortaleza.release();
+                        MinhaFortaleza=MediaPlayer.create(context,R.raw.minhafortaleza);
+                    } MinhaFortaleza.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        btnOrando.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    if (Orando.isPlaying()) {
+                        Orando.stop();
+                        Orando.release();
+                        Orando=MediaPlayer.create(context,R.raw.orando);
+                    } Orando.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        btnDocePaz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    if (DocePaz.isPlaying()) {
+                        DocePaz.stop();
+                        DocePaz.release();
+                        DocePaz=MediaPlayer.create(context,R.raw.docepaz);
+                    } DocePaz.start();
+                } catch(Exception e) { e.printStackTrace(); }
+            }
+        });
+        btnOucoSuaVoz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    if (OucoSuaVoz.isPlaying()) {
+                        OucoSuaVoz.stop();
+                        OucoSuaVoz.release();
+                        OucoSuaVoz=MediaPlayer.create(context,R.raw.oucosuavoz);
+                    } OucoSuaVoz.start();
                 } catch(Exception e) { e.printStackTrace(); }
             }
         });
@@ -54,16 +132,15 @@ public class MainActivity extends AppCompatActivity {
         stop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                    if (mp2.isPlaying()||mp.isPlaying()) {
-                        mp.stop();
-                        mp2.stop();
-                        mp.release();
-                        mp2.release();
-                        mp = MediaPlayer.create(context, R.raw.sound);
-                        mp2 = MediaPlayer.create(context, R.raw.sound2);
-                    }
+                EuCreio.stop();EuCreio.release();EuCreio=MediaPlayer.create(context,R.raw.eucreio);
+                Consagracao.stop();Consagracao.release();Consagracao=MediaPlayer.create(context,R.raw.consagracao);
+                UltimoConvite.stop();UltimoConvite.release();UltimoConvite=MediaPlayer.create(context,R.raw.ultimoconvite);
+                MinhaFortaleza.stop();MinhaFortaleza.release();MinhaFortaleza=MediaPlayer.create(context,R.raw.minhafortaleza);
+                Orando.stop();Orando.release();Orando=MediaPlayer.create(context,R.raw.orando);
+                DocePaz.stop();DocePaz.release();DocePaz=MediaPlayer.create(context,R.raw.docepaz);
+                OucoSuaVoz.stop();OucoSuaVoz.release();OucoSuaVoz=MediaPlayer.create(context,R.raw.oucosuavoz);
             }
         });
+
     }
 }
